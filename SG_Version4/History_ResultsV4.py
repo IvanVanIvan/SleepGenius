f3f98b8aeff956fb897cb05ab89665d807079dfe
IvanVanIvan <36544648+IvanVanIvan@users.noreply.github.com>
2023-08-13 23:33:02 +1200

    Too long lines rewritten in Result_PageV4.py

--- SG_Version4/History_ResultsV4.py
+++ SG_Version4/History_ResultsV4.py
@@ -19,49 +19,112 @@ class HistoryResults:
         self.HistoryScreen.configure(bg="black")
 
         # Load and display the image
-        # open image 
-        self.image = Image.open("assets\LogoY13.png") 
+        # open image
+        self.image = Image.open("assets\\LogoY13.png")
         # Resize the image to 100x100 pixels
-        self.image = self.image.resize((75, 75)) 
+        self.image = self.image.resize((75, 75))
         # Convert the image to a format compatible with Tkinter
         self.image_tk = ImageTk.PhotoImage(self.image)
         # Create a label to display the image
-        self.image_label = tk.Label(self.HistoryScreen, image=self.image_tk, bg="black")
+        self.image_label = tk.Label(
+            self.HistoryScreen,
+            image=self.image_tk,
+            bg="black")
         # Set the position of the image label
         self.image_label.place(x=275, y=5)
 
         # Create a label to display the app name
-        self.app_label = tk.Label(self.HistoryScreen, text="Sleep \n Genius", bg="black", fg="#FFDE59", width="6", height="2", font=("Blinker", 15, "bold"))
+        self.app_label = tk.Label(
+            self.HistoryScreen,
+            text="Sleep \n Genius",
+            bg="black",
+            fg="#FFDE59",
+            width="6",
+            height="2",
+            font=(
+                "Blinker",
+                15,
+                "bold"))
         # Set the position of the label
         self.app_label.place(x=200, y=15)
 
         # Create a label to display "Your"
-        self.Your_label = tk.Label(self.HistoryScreen, text="Your", bg="black", fg="white", width="8", height="2", font=("Blinker", 20, "bold"))
+        self.Your_label = tk.Label(
+            self.HistoryScreen,
+            text="Your",
+            bg="black",
+            fg="white",
+            width="8",
+            height="2",
+            font=(
+                "Blinker",
+                20,
+                "bold"))
         # Set the position of the label
         self.Your_label.place(x=25, y=85)
 
         # Create a label to display "Results"
-        self.Results_label = tk.Label(self.HistoryScreen, text="Results", bg="black", fg="#FFDE59", width="10", height="2", font=("Blinker", 20, "bold"))
+        self.Results_label = tk.Label(
+            self.HistoryScreen,
+            text="Results",
+            bg="black",
+            fg="#FFDE59",
+            width="10",
+            height="2",
+            font=(
+                "Blinker",
+                20,
+                "bold"))
         # Set the position of the label
         self.Results_label.place(x=25, y=135)
 
         # Create a label to display message
-        self.message_label = tk.Label(self.HistoryScreen, text="Sleep results from this week", bg="black", fg="#0CC0DF")
+        self.message_label = tk.Label(
+            self.HistoryScreen,
+            text="Sleep results from this week",
+            bg="black",
+            fg="#0CC0DF")
         # Set the position of the label
         self.message_label.place(x=60, y=200)
 
         # Create the button to Logout
-        self.logout_button = tk.Button(self.HistoryScreen, text="Logout", bg="#0CC0DF", fg="white", height="2", width="11", cursor="hand2", command = self.Logout)
+        self.logout_button = tk.Button(
+            self.HistoryScreen,
+            text="Logout",
+            bg="#0CC0DF",
+            fg="white",
+            height="2",
+            width="11",
+            cursor="hand2",
+            command=self.Logout)
         # Set the position of the button
         self.logout_button.place(x=20, y=20)
 
         # switch to Input page message
-        self.msgInptP_label = tk.Label(self.HistoryScreen, text="Input another result", bg="black", fg="#0CC0DF", width="20", height="1", font=("Blinker", 10, "bold"))
+        self.msgInptP_label = tk.Label(
+            self.HistoryScreen,
+            text="Input another result",
+            bg="black",
+            fg="#0CC0DF",
+            width="20",
+            height="1",
+            font=(
+                "Blinker",
+                10,
+                "bold"))
         # Set the position of the label
         self.msgInptP_label.place(x=30, y=440)
 
         # Create the button to input another sleep result
-        self.logout_button = tk.Button(self.HistoryScreen, text="here", bg="#FFDE59", fg="#0CC0DF", height="1", width="10", cursor="hand2", command = self.input_another_result)
+        self.logout_button = tk.Button(
+            self.HistoryScreen,
+            text="here",
+            bg="#FFDE59",
+            fg="#0CC0DF",
+            height="1",
+            width="10",
+            cursor="hand2",
+            command=self.input_another_result)
         # Set the position of the button
         self.logout_button.place(x=190, y=440)
 
@@ -76,7 +139,8 @@ class HistoryResults:
         cursor = conn.cursor()
 
         # select all data from the table to check for existing data
-        cursor.execute("SELECT * FROM UsersSleepData WHERE User_Name=?", (username,))
+        cursor.execute(
+            "SELECT * FROM UsersSleepData WHERE User_Name=?", (username,))
         # get the rows
         row = cursor.fetchone()
         # close database
@@ -85,36 +149,62 @@ class HistoryResults:
         if row:
             # now using a for loop
             # Check if there's data for each column (hours slept)
-            for i in range(1, 8):  # Assuming there are 7 columns for hours slept (from 1 to 7)
+            for i in range(1, 8):
+                # Assuming there are 7 columns for hours slept (from 1 to 7)
                 # Check if the column is not NULL
                 if row[i] is not None:
                     # Only create a label if the cell has data
                     hours_label = "Hours slept: " + str(row[i])
                     # Create a label for the message
-                    self.sleep_label = tk.Label(self.HistoryScreen, text=hours_label, bg="black", fg="white", highlightthickness=2, highlightbackground="#FFDE59", font=("Blinker", 8, "bold"))
+                    self.sleep_label = tk.Label(
+                        self.HistoryScreen,
+                        text=hours_label,
+                        bg="black",
+                        fg="white",
+                        highlightthickness=2,
+                        highlightbackground="#FFDE59",
+                        font=(
+                            "Blinker",
+                            8,
+                            "bold"))
                     # Set the position of the label
                     self.sleep_label.place(x=55, y=200 + i * 30)
         else:
             # Display "No sleep Data entered this week" message
-            self.no_data_label = tk.Label(self.HistoryScreen, text="No sleep Data entered this week", bg="black", fg="white", highlightthickness=2, highlightbackground="#FFDE59", font=("Blinker", 8, "bold"))
+            self.no_data_label = tk.Label(
+                self.HistoryScreen,
+                text="No sleep Data entered this week",
+                bg="black",
+                fg="white",
+                highlightthickness=2,
+                highlightbackground="#FFDE59",
+                font=(
+                    "Blinker",
+                    8,
+                    "bold"))
             # Set the position of the label
             self.no_data_label.place(x=55, y=240)
 
-    # function that will clear the text file, used when loging out and when switching a file thats a step back
+    # function that will clear the text file, used when loging out and when
+    # switching a file thats a step back
     def clearHoursTXT(self):
         # open the txt file in write mode
-        with open("txt_Files\HourInput.txt", "w") as hoursSlept_file:
-            # truncating the file to a size of 0 bytes, which effectively deletes the contents of the file
+        with open("txt_Files\\HourInput.txt", "w") as hoursSlept_file:
+            # truncating the file to a size of 0 bytes, which effectively
+            # deletes the contents of the file
             hoursSlept_file.truncate(0)
-    
-    # function that will clear the text file, used when loging out and when switching a file thats a step back
+
+    # function that will clear the text file, used when loging out and when
+    # switching a file thats a step back
     def clearUNameTXT(self):
         # open the txt file in write mode
         with open(r"txt_Files\UserName.txt", "w") as username_file:
-            # truncating the file to a size of 0 bytes, which effectively deletes the contents of the file
+            # truncating the file to a size of 0 bytes, which effectively
+            # deletes the contents of the file
             username_file.truncate(0)
 
-    # function that will delete the data for a user when loging out, only when all the coloumns are filled
+    # function that will delete the data for a user when loging out, only when
+    # all the coloumns are filled
     def delete_user_if_all_data_exists(self):
         # Read username from "UserName.txt"
         with open(r"txt_Files\UserName.txt", "r") as username_file:
@@ -134,22 +224,26 @@ class HistoryResults:
         # check if every cell is is filled
         if hours_data is not None and all(hours_data):
             # Convert to float and filter out invalid values
-            valid_hours_data = [float(value) for value in hours_data if isinstance(value, (int, float, str)) and value.strip()]
-            
+            valid_hours_data = [
+                float(value) for value in hours_data if isinstance(
+                    value, (int, float, str)) and value.strip()]
+
             # Calculate average sleep
             average_sleep = sum(valid_hours_data) / len(valid_hours_data)
 
             # Show a message box with the average sleep
-            messagebox.showinfo("Average Sleep", f"Your average sleep for this week was: {average_sleep:.2f} hours")
+            messagebox.showinfo(
+                "Average Sleep",
+                f"Your average sleep for this week was: {average_sleep:.2f} hours")
 
             # All Hours_Slept columns have data, delete the user's data
             delete_query = 'DELETE FROM UsersSleepData WHERE User_Name = ?'
             cursor.execute(delete_query, (username,))
             conn.commit()
         else:
-            # do nothing if there are still empty Hours_Slept coloumbs 
+            # do nothing if there are still empty Hours_Slept coloumbs
             pass
-        
+
         # close database
         conn.close()
 
@@ -160,7 +254,7 @@ class HistoryResults:
         # clear the hours txt file
         self.clearHoursTXT()
         # Open the SG_Version4\SleepData_InputV4.py file using subprocess
-        subprocess.Popen(["python", "SG_Version4\SleepData_InputV4.py"])
+        subprocess.Popen(["python", "SG_Version4\\SleepData_InputV4.py"])
         # Close the HistoryScreen screen
         self.HistoryScreen.destroy()
 
@@ -173,13 +267,14 @@ class HistoryResults:
         # clear the data from username txt file
         self.clearUNameTXT()
         # Open the SG_Version4\landingV4.py file using subprocess
-        subprocess.Popen(["python", "SG_Version4\landingV4.py"])
+        subprocess.Popen(["python", "SG_Version4\\landingV4.py"])
         # Close the HistoryScreen screen
         self.HistoryScreen.destroy()
 
     def run(self):
         # This line starts the event loop and keeps the application running.
         self.HistoryScreen.mainloop()
+
 
 if __name__ == "__main__":
     # Create an instance of the LandingPage class
--- SG_Version4/SleepData_InputV4.py
+++ SG_Version4/SleepData_InputV4.py
@@ -17,46 +17,81 @@ class InputPage:
         # Set the background color to black
         self.Input_screen.configure(bg="black")
 
-
         # Load and display the image
         # open image
-        self.image = Image.open("assets\LogoY13.png")
+        self.image = Image.open("assets\\LogoY13.png")
         # Resize the image to 75x75 pixels
-        self.image = self.image.resize((75, 75)) 
+        self.image = self.image.resize((75, 75))
         # Convert the image to a format compatible with Tkinter
         self.image_tk = ImageTk.PhotoImage(self.image)
         # Create a label to display the image
-        self.image_label = tk.Label(self.Input_screen, image=self.image_tk, bg="black")
+        self.image_label = tk.Label(
+            self.Input_screen,
+            image=self.image_tk,
+            bg="black")
         # Set the position of the image label
-        self.image_label.place(x=275, y=5)  
-
+        self.image_label.place(x=275, y=5)
 
         # Create a label to display the app name
-        self.app_label = tk.Label(self.Input_screen, text="Sleep \n Genius", bg="black", fg="#FFDE59", width="6", height="2", font=("Blinker", 15, "bold"))
+        self.app_label = tk.Label(
+            self.Input_screen,
+            text="Sleep \n Genius",
+            bg="black",
+            fg="#FFDE59",
+            width="6",
+            height="2",
+            font=(
+                "Blinker",
+                15,
+                "bold"))
         # Set the position of the label
         self.app_label.place(x=200, y=15)
 
-
         # Create a label to display the let's message
-        self.lets_label = tk.Label(self.Input_screen, text="Let's", bg="black", fg="white", width="10", height="2", font=("Blinker", 20, "bold"))
+        self.lets_label = tk.Label(
+            self.Input_screen,
+            text="Let's",
+            bg="black",
+            fg="white",
+            width="10",
+            height="2",
+            font=(
+                "Blinker",
+                20,
+                "bold"))
         # Set the position of the label
         self.lets_label.place(x=5, y=100)
 
-
         # Create a label to display get started message
-        self.start_label = tk.Label(self.Input_screen, text="Get started", bg="black", fg="#FFDE59", width="10", height="2", font=("Blinker", 20, "bold"))
+        self.start_label = tk.Label(
+            self.Input_screen,
+            text="Get started",
+            bg="black",
+            fg="#FFDE59",
+            width="10",
+            height="2",
+            font=(
+                "Blinker",
+                20,
+                "bold"))
         # Set the position of the label
         self.start_label.place(x=45, y=150)
 
-
         # Create a label to display the message
-        self.message_label = tk.Label(self.Input_screen, text="Please enter your sleep data below", bg="black", fg="#0CC0DF")
+        self.message_label = tk.Label(
+            self.Input_screen,
+            text="Please enter your sleep data below",
+            bg="black",
+            fg="#0CC0DF")
         # Set the position of the label
         self.message_label.place(x=60, y=220)
 
-
         # Create a label to display number of hrs message
-        self.numHrs_label = tk.Label(self.Input_screen, text="Number of hours \n slept* ", bg="black", fg="white")
+        self.numHrs_label = tk.Label(
+            self.Input_screen,
+            text="Number of hours \n slept* ",
+            bg="black",
+            fg="white")
         # Set the position of the label
         self.numHrs_label.place(x=50, y=290)
         # Create an Entry widget
@@ -65,27 +100,61 @@ class InputPage:
         self.hours_entry.place(x=160, y=290)
 
         # Create the button to Logout
-        self.logout_button = tk.Button(self.Input_screen, text="Logout", bg="#0CC0DF", fg="white", height="2", width="11", cursor="hand2", command = self.Logout)
+        self.logout_button = tk.Button(
+            self.Input_screen,
+            text="Logout",
+            bg="#0CC0DF",
+            fg="white",
+            height="2",
+            width="11",
+            cursor="hand2",
+            command=self.Logout)
         # Set the position of the button
         self.logout_button.place(x=20, y=20)
 
         # Create Submit button
-        self.submit_button = tk.Button(self.Input_screen, text="Submit", bg="#0CC0DF", fg="white", height="2", width="30", cursor="hand2", command= self.save_sleep_data)
+        self.submit_button = tk.Button(
+            self.Input_screen,
+            text="Submit",
+            bg="#0CC0DF",
+            fg="white",
+            height="2",
+            width="30",
+            cursor="hand2",
+            command=self.save_sleep_data)
         # Set the position of the button
         self.submit_button.place(x=50, y=380)
 
         # switch to History page message
-        self.msgHistP_label = tk.Label(self.Input_screen, text="View previous result", bg="black", fg="#0CC0DF", width="20", height="1", font=("Blinker", 10, "bold"))
+        self.msgHistP_label = tk.Label(
+            self.Input_screen,
+            text="View previous result",
+            bg="black",
+            fg="#0CC0DF",
+            width="20",
+            height="1",
+            font=(
+                "Blinker",
+                10,
+                "bold"))
         # Set the position of the label
         self.msgHistP_label.place(x=30, y=440)
 
         # Create the button to switch to History page
-        self.switchHisP_button = tk.Button(self.Input_screen, text="Here", bg="#FFDE59", fg="#0CC0DF", height="1", width="10", cursor="hand2", command = self.open_History_page)
+        self.switchHisP_button = tk.Button(
+            self.Input_screen,
+            text="Here",
+            bg="#FFDE59",
+            fg="#0CC0DF",
+            height="1",
+            width="10",
+            cursor="hand2",
+            command=self.open_History_page)
         # Set the position of the button
         self.switchHisP_button.place(x=190, y=440)
-        
 
-    # save sleep data function 
+    # save sleep data function
+
     def save_sleep_data(self):
         try:
             hours_slept = float(self.hours_entry.get())
@@ -96,59 +165,67 @@ class InputPage:
                 messagebox.showinfo("Success", "Your result has been recorded")
 
                 # record the hours into text file for later use
-                with open("txt_Files\HourInput.txt", "w") as hours_file:
+                with open("txt_Files\\HourInput.txt", "w") as hours_file:
                     hours_file.write(f"{hours_slept}")
 
                 # open the result file
                 self.open_result_page()
             else:
                 # show error message when the number is too big
-                messagebox.showwarning("Invalid Input", "The number of hours slept should be between 1 and 24.")
+                messagebox.showwarning(
+                    "Invalid Input",
+                    "The number of hours slept should be between 1 and 24.")
         except ValueError:
             # show error message when user doesnt input a number
-            messagebox.showerror("Invalid Input", "Please enter a valid number.")
+            messagebox.showerror(
+                "Invalid Input",
+                "Please enter a valid number.")
 
-    
-    # function that will clear the text file, used when loging out and when switching a file thats a step back
+    # function that will clear the text file, used when loging out and when
+    # switching a file thats a step back
+
     def clearHoursTXT(self):
         # open the txt file in write mode
-        with open("txt_Files\HourInput.txt", "w") as hoursSlept_file:
-            # truncating the file to a size of 0 bytes, which effectively deletes the contents of the file
+        with open("txt_Files\\HourInput.txt", "w") as hoursSlept_file:
+            # truncating the file to a size of 0 bytes, which effectively
+            # deletes the contents of the file
             hoursSlept_file.truncate(0)
-    
-    # function that will clear the text file, used when loging out and when switching a file thats a step back
+
+    # function that will clear the text file, used when loging out and when
+    # switching a file thats a step back
     def clearUNameTXT(self):
         # open the txt file in write mode
         with open(r"txt_Files\UserName.txt", "w") as username_file:
-            # truncating the file to a size of 0 bytes, which effectively deletes the contents of the file
+            # truncating the file to a size of 0 bytes, which effectively
+            # deletes the contents of the file
             username_file.truncate(0)
-    
 
     def open_result_page(self):
         # Open the SG_Version4\Result_PageV4.py file using subprocess
-        subprocess.Popen(["python", "SG_Version4\Result_PageV4.py"])
+        subprocess.Popen(["python", "SG_Version4\\Result_PageV4.py"])
         # Close the main screen
         self.Input_screen.destroy()
-    
+
     # open the history page
     def open_History_page(self):
         # Open the SG_Version4\History_ResultsV4.py file using subprocess
-        subprocess.Popen(["python", "SG_Version4\History_ResultsV4.py"])
+        subprocess.Popen(["python", "SG_Version4\\History_ResultsV4.py"])
         # Close the main screen
         self.Input_screen.destroy()
-    
+
     # function to logout
     def Logout(self):
         # delete data from username txt file
         self.clearUNameTXT()
         # Open the SG_Version4\landingV4.py file using subprocess
-        subprocess.Popen(["python", "SG_Version4\landingV4.py"])
+        subprocess.Popen(["python", "SG_Version4\\landingV4.py"])
         # Close the Input_screen screen
         self.Input_screen.destroy()
 
     def run(self):
         # This line starts the event loop and keeps the application running.
         self.Input_screen.mainloop()
+
 
 if __name__ == "__main__":
     # Create an instance of the InputPage class
